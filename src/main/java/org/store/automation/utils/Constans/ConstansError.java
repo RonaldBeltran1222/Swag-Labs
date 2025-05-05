ee7c69239b23fc5617c893fb8285bb16a80846a9
RonaldBeltran1222 <alexanderbeltran2208@outlook.com>
2025-05-05 17:22:21 -0500

    update README.md v2

---
+++ src/main/java/org/store/automation/utils/Constans/ConstansError.java
@@ -0,0 +1,5 @@
+package utils.Constans;
+
+public class ConstansError {
+    public static final String ERROR_LOGIN = "El login no fue exitoso.";
+}
---
+++ src/main/java/org/store/automation/utils/Localizadores.java
@@ -0,0 +1,10 @@
+package utils.Locators;
+
+public class Localizadores {
+
+    public static final String USERNAME_FIELD = "user-name";
+    public static final String PASSWORD_FIELD = "password";
+    public static final String LOGIN_BUTTON   = "login-button";
+    public static final String ERROR_MESSAGE  = "h3[data-test='error']";
+
+}
